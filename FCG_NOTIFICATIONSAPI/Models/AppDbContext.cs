35e82700ae2523246362688e6fe95c07472a6088
natha <natha@NATHALIALO>
2026-01-11 15:32:06 -0300

    feat: first version

---
+++ FCG_NOTIFICATIONSAPI/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FCG_NOTIFICATIONSAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //public DbSet<Game> Games { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Game>().HasKey(g => g.IdGames);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
